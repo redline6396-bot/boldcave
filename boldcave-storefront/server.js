ddc273875a49991dd4ba03a726a2013a4ee53fa0
Kush Gupta <kushtech123@gmail.com>
2026-09-01 00:26:54 +0530

    yeah

--- boldcave-storefront/server.js
+++ boldcave-storefront/server.js
@@ -1,7 +1,7 @@
 const { createServer } = require("node:http");
 const next = require("next");
 
-const dev = process.env.NODE_ENV !== "production";
+const dev = process.env.NODE_ENV === "development";
 const hostname = process.env.HOST || "0.0.0.0";
 const port = Number(process.env.PORT || 3000);
 
