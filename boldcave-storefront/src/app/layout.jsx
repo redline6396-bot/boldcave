d08fdc04345ccb8a4df16d5cd5ffe5e0fd6d8148
kush01007 <kushtech123@gmail.com>
2026-09-17 21:19:58 +0530

    meta

--- boldcave-storefront/src/app/layout.jsx
+++ boldcave-storefront/src/app/layout.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Script from "next/script";
 import AuthProvider from '@/context/AuthContext';
 import CartProvider from '@/context/CartContext';
 import CouponProvider from '@/context/CouponContext';
@@ -75,6 +76,29 @@ export default async function RootLayout({ children }) {
           <link rel="icon" href={APP_ICON_PATH} />
         </head>
         <body>
+          <Script id="meta-pixel" strategy="afterInteractive">
+            {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1061596686650693');
+              fbq('track', 'PageView');
+            `}
+          </Script>
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              src="https://www.facebook.com/tr?id=1061596686650693&ev=PageView&noscript=1"
+              alt=""
+            />
+          </noscript>
           <ComingSoonScreen />
         </body>
       </html>
@@ -90,6 +114,29 @@ export default async function RootLayout({ children }) {
         <link rel="icon" href={APP_ICON_PATH} />
       </head>
       <body>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1061596686650693');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1061596686650693&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <NotificationProvider>
           <AuthProvider>
             <CartProvider>
